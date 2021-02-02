af468f084ecb6d0035fcef1697f3bd60d934f2e4
Francisco Sanchez <francisco.sanchez@salesforce.com>
2021-02-02 14:02:15 -0800

    Add fonts

---
+++ Classes/SceneMenu.cpp
@@ -0,0 +1,8 @@
+//
+//  SceneMenu.cpp
+//  cocos2d
+//
+//  Created by Francisco Sanchez on 2/2/21.
+//
+
+#include "SceneMenu.hpp"
---
+++ Classes/SceneMenu.h
@@ -0,0 +1,13 @@
+//
+//  SceneMenu.hpp
+//  cocos2d
+//
+//  Created by Francisco Sanchez on 2/2/21.
+//
+
+#ifndef SceneMenu_hpp
+#define SceneMenu_hpp
+
+#include <stdio.h>
+
+#endif /* SceneMenu_hpp */
